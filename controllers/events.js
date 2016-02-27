0824431fce6f66403f1e7da8c6eb55533b5415d6
Krystian Kościelniak <krystiankoscielniak@gmail.com>
2016-02-28 00:01:06 +0100

    rooms logic WIP: creating

--- controllers/events.js
+++ controllers/events.js
@@ -2,6 +2,7 @@
  * socket.io events
  */
 var uuid = require('node-uuid');
+var Room = require('./room');
 
 Array.prototype.contains = function(k, callback) {
     var self = this;
@@ -22,7 +23,7 @@ exports = module.exports = function (io) {
         rooms = {},
         clients = [];
 
-    io.on('connection', function(client) {
+    io.sockets.on('connection', function(client) {
 
         client.on('join', function(name) {
             var roomID = null;
@@ -36,158 +37,161 @@ exports = module.exports = function (io) {
         });
 
         client.on('createRoom', function(name) {
-            if (people[client.id].room === null) {
-                var uniqueId = uuid.v4();
 
+            if (people[client.id].room === null) {
+
+                var uniqueId = uuid.v4();
                 var room = new Room(name, uniqueId, client.id);
 
                 rooms[uniqueId] = room;
 
-                io.sockets.emit('roomList', { rooms: rooms });
-                client.room = name;
+                io.sockets.emit('roomList', rooms);
+                console.log(rooms);
+
+                client.room = room.name;
                 client.join(client.room);
                 room.addPerson(client.uniqueId);
 
-                people[client.uniqueId].room = uniqueId;
-
-                console.log(uniqueId);
+                people[client.id].room = uniqueId;
             } else {
                 io.sockets.emit('update', 'You have already created a room');
             }
         });
 
         client.on('joinRoom', function(roomId) {
-            var room = rooms[roomId];
+            // console.log(client.id);
 
-            if (client.id === room.owner) {
-                client.emit('update', 'You are the owner of this room and you\'ve already joined');
-            } else {
-                room.people.contains(client.id, function(found) {
-                    if (found) {
-                        client.emit('update', 'You have already joined this room.');
-                    } else {
-                        if (people[client.id].inroom !== null) {
-                            client.emit('update', 'You are already in a room (' +
-                                rooms[people[client.id].inroom].name +
-                                '), please leave it first to join another room.');
-                        } else {
-                            room.addPerson(client.id);
-                            people[client.id].inroom = uniqueId;
-                            client.room = room.name;
-                            client.join(client.room);
-                            user = people[client.id];
-                            socket.sockets.in(client.room).emit('update', user.name + ' has connected to ' + room.name + ' room.');
-                            client.emit('update', 'Welcome to ' + room.name + '.');
-                            client.emit('sendRoomID', { id: id });
-                        }
-                    }
-                });
-            }
+            // var room = rooms[roomId];
+            //
+            // if (client.id === room.owner) {
+            //     client.emit('update', 'You are the owner of this room and you\'ve already joined');
+            // } else {
+            //     room.people.contains(client.id, function(found) {
+            //         if (found) {
+            //             client.emit('update', 'You have already joined this room.');
+            //         } else {
+            //             if (people[client.id].inroom !== null) {
+            //                 client.emit('update', 'You are already in a room (' +
+            //                     rooms[people[client.id].inroom].name +
+            //                     '), please leave it first to join another room.');
+            //             } else {
+            //                 room.addPerson(client.id);
+            //                 people[client.id].inroom = uniqueId;
+            //                 client.room = room.name;
+            //                 client.join(client.room);
+            //                 user = people[client.id];
+            //                 socket.sockets.in(client.room).emit('update', user.name + ' has connected to ' + room.name + ' room.');
+            //                 client.emit('update', 'Welcome to ' + room.name + '.');
+            //                 client.emit('sendRoomID', { id: id });
+            //             }
+            //         }
+            //     });
+            // }
         });
 
-        client.on('send', function(msg) {
-            if (socket.sockets.manager.roomClients[client.id]['/' + client.room] !== undefined ) {
-                    socket.sockets.in(client.room).emit('chat', people[client.id], msg);
-            } else {
-                client.emit('update', 'Please connect to a room.');
-            }
-        });
-
-        client.on('leaveRoom', function(id) {
-            var room = rooms[id];
-
-            if (client.id === room.owner) {
-                var i = 0;
-                while (i < clients.length) {
-                    if (clients[i].id === room.people[i]) {
-                        people[clients[i].id].inroom = null;
-                        clients[i].leave(room.name);
-                    }
-                    ++i;
-                }
-                delete rooms[id];
-                people[room.owner].owns = null;
-
-                io.sockets.emit('roomList', { rooms: rooms });
-
-                io.sockets.in(client.room).emit('update', 'The owner (' +
-                    user.name + ') is leaving the room. The room is removed');
-
-            } else {
-                room.people.contains(client.id, function(found) {
-                    if (found) {
-                        var personIndex = room.people.indexOf(client.id);
-                        room.people.splice(personIndex, 1);
-                        io.sockets.emit('update', people[client.id].name +
-                            ' has left the room.');
-
-                        client.leave(room.name);
-                    }
-                });
-            }
-        });
-
-        client.on('removeRoom', function(id) {
-            var room = rooms[id];
-            if (room) {
-                if (client.id === room.owner) {
-                    var personCount = room.people.length;
-
-                    if (personCount > 2) {
-                        console.log('there are still people in the room warning'); // @TODO
-                    } else {
-                        if (client.id === room.owner) {
-                            socket.sockets.in(client.room).emit('update', 'The owner (' + people[client.id].name + ') removed the room.');
-
-                            var i = 0;
-
-                            while (i < clients.length) {
-
-                                if(clients[i].id === room.people[i]) {
-                                    people[clients[i].id].inroom = null;
-                                    clients[i].leave(room.name);
-                                }
-                                ++i;
-                            }
-                            delete rooms[id];
-                            people[room.owner].owns = null;
-                            socket.sockets.emit('roomList', {rooms: rooms});
-
-                        }
-                    }
-                } else {
-                    client.emit('update', 'Only the owner can remove a room.');
-                }
-            }
-        });
-
-        client.on('disconnect', function(room) {
-            if (people[client.id]) {
-                if (people[client.id].inroom === null) {
-                    socket.sockets.emit('update', people[client.id].name + ' has left the server.');
-                    delete people[client.id];
-                    socket.sockets.emit('updatePeople', people);
-                } else {
-                    if (people[client.id].owns !== null) {
-                        var room = rooms[people[client.id].owns];
-                        if (client.id === room.owner) {
-                            var i = 0;
-                            while(i < clients.length) {
-                                if (clients[i].id === room.people[i]) {
-                                    people[clients[i].id].inroom = null;
-                                    clients[i].leave(room.name);
-                                }
-                                ++i;
-                            }
-                            delete rooms[people[client.id].owns];
-                        }
-                    }
-                    socket.sockets.emit('update', people[client.id].name + ' has left the server.');
-                    delete people[client.id];
-                    socket.sockets.emit('updatePeople', people);
-                    socket.sockets.emit('roomList', {rooms: rooms});
-                }
-            }
-        });
+        // client.on('send', function(msg) {
+        //     if (socket.sockets.manager.roomClients[client.id]['/' + client.room] !== undefined ) {
+        //             socket.sockets.in(client.room).emit('chat', people[client.id], msg);
+        //     } else {
+        //         client.emit('update', 'Please connect to a room.');
+        //     }
+        // });
+        //
+        // client.on('leaveRoom', function(id) {
+        //     var room = rooms[id];
+        //
+        //     if (client.id === room.owner) {
+        //         var i = 0;
+        //         while (i < clients.length) {
+        //             if (clients[i].id === room.people[i]) {
+        //                 people[clients[i].id].inroom = null;
+        //                 clients[i].leave(room.name);
+        //             }
+        //             ++i;
+        //         }
+        //         delete rooms[id];
+        //         people[room.owner].owns = null;
+        //
+        //         io.sockets.emit('roomList', { rooms: rooms });
+        //
+        //         io.sockets.in(client.room).emit('update', 'The owner (' +
+        //             user.name + ') is leaving the room. The room is removed');
+        //
+        //     } else {
+        //         room.people.contains(client.id, function(found) {
+        //             if (found) {
+        //                 var personIndex = room.people.indexOf(client.id);
+        //                 room.people.splice(personIndex, 1);
+        //                 io.sockets.emit('update', people[client.id].name +
+        //                     ' has left the room.');
+        //
+        //                 client.leave(room.name);
+        //             }
+        //         });
+        //     }
+        // });
+        //
+        // client.on('removeRoom', function(id) {
+        //     var room = rooms[id];
+        //     if (room) {
+        //         if (client.id === room.owner) {
+        //             var personCount = room.people.length;
+        //
+        //             if (personCount > 2) {
+        //                 console.log('there are still people in the room warning'); // @TODO
+        //             } else {
+        //                 if (client.id === room.owner) {
+        //                     socket.sockets.in(client.room).emit('update', 'The owner (' + people[client.id].name + ') removed the room.');
+        //
+        //                     var i = 0;
+        //
+        //                     while (i < clients.length) {
+        //
+        //                         if(clients[i].id === room.people[i]) {
+        //                             people[clients[i].id].inroom = null;
+        //                             clients[i].leave(room.name);
+        //                         }
+        //                         ++i;
+        //                     }
+        //                     delete rooms[id];
+        //                     people[room.owner].owns = null;
+        //                     socket.sockets.emit('roomList', {rooms: rooms});
+        //
+        //                 }
+        //             }
+        //         } else {
+        //             client.emit('update', 'Only the owner can remove a room.');
+        //         }
+        //     }
+        // });
+        //
+        // client.on('disconnect', function(room) {
+        //     if (people[client.id]) {
+        //         if (people[client.id].inroom === null) {
+        //             socket.sockets.emit('update', people[client.id].name + ' has left the server.');
+        //             delete people[client.id];
+        //             socket.sockets.emit('updatePeople', people);
+        //         } else {
+        //             if (people[client.id].owns !== null) {
+        //                 var room = rooms[people[client.id].owns];
+        //                 if (client.id === room.owner) {
+        //                     var i = 0;
+        //                     while(i < clients.length) {
+        //                         if (clients[i].id === room.people[i]) {
+        //                             people[clients[i].id].inroom = null;
+        //                             clients[i].leave(room.name);
+        //                         }
+        //                         ++i;
+        //                     }
+        //                     delete rooms[people[client.id].owns];
+        //                 }
+        //             }
+        //             socket.sockets.emit('update', people[client.id].name + ' has left the server.');
+        //             delete people[client.id];
+        //             socket.sockets.emit('updatePeople', people);
+        //             socket.sockets.emit('roomList', {rooms: rooms});
+        //         }
+        //     }
+        // });
     });
 }
--- public/assets/js/client.js
+++ public/assets/js/client.js
@@ -1,112 +1,115 @@
-(function() {
-
-    var $window = $(window),
-        $body = $('body'),
-        $loginModal = $body.find('.js-login-modal'),
-        $usersTable = $body.find('.js-users-table');
-
-    var socket = io();
-
-
-    var currentUser = {};
-
-
-
-    $window.load(function() {
-        $('#login-modal').modal('show');
-    });
-
-    if ($loginModal.length) {
-
-        $loginModal.find('.js-join').on('click', function(e) {
-
-            var username = $loginModal.find('.js-username').val();
-            currentUser.username = username;
-            socket.emit('join', username);
-            $loginModal.modal('hide');
-        });
-    }
-})(jQuery);
-
-
-
-
-
-// angular attemtp
-
-
+/*
+* Angular part of the app
+*/
 var brawl = angular.module('brawl', []);
 
 brawl.factory('socket', function ($rootScope) {
-  var socket = io.connect();
-  return {
-    on: function (eventName, callback) {
-      socket.on(eventName, function () {
-        var args = arguments;
-        $rootScope.$apply(function () {
-          callback.apply(socket, args);
-        });
-      });
-    },
-    emit: function (eventName, data, callback) {
-      socket.emit(eventName, data, function () {
-        var args = arguments;
-        $rootScope.$apply(function () {
-          if (callback) {
-            callback.apply(socket, args);
-          }
-        });
-      })
-    }
-  };
+    var socket = io.connect();
+
+    socket.on('update', function(data) {
+        console.log(data);
+    });
+
+    return {
+        on: function (eventName, callback) {
+            socket.on(eventName, function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    callback.apply(socket, args);
+                });
+            });
+        },
+        emit: function (eventName, data, callback) {
+            socket.emit(eventName, data, function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    if (callback) {
+                        callback.apply(socket, args);
+                    }
+                });
+            })
+        }
+    };
 });
+
+brawl.controller('loginController', ['$scope', 'socket', function($scope, socket) {
+
+    $scope.showModal = true;
+    $scope.rooms = [];
+
+    $scope.join = function(name) {
+        socket.emit('join', name);
+        $scope.showModal = !$scope.showModal;
+    };
+}]);
+
+
 
 
 brawl.controller('usersController', ['$scope', 'socket', function($scope, socket) {
 
     $scope.users = [];
+    $scope.rooms = [];
+    $scope.showModal = false;
+
+    $scope.toggleModal = function() {
+        $scope.showModal = !$scope.showModal;
+    };
+
+    $scope.createRoom = function(newRoomName) {
+
+        socket.emit('createRoom', newRoomName);
+        $scope.showModal = !$scope.showModal;
+    }
 
     socket.on('updatePeople', function(people) {
         $scope.users = people;
     });
+
+    socket.on('roomList', function(rooms) {
+        $scope.rooms = rooms;
+    });
 }]);
 
-// function usersController($scope, $http) {
-//     $scope.formData = {};
-//
-//     // when landing on the page, get all todos and show them
-//     // $http.get('/api/todos')
-//     //     .success(function(data) {
-//     //         $scope.todos = data;
-//     //         console.log(data);
-//     //     })
-//     //     .error(function(data) {
-//     //         console.log('Error: ' + data);
-//     //     });
-//
-//     // // when submitting the add form, send the text to the node API
-//     // $scope.createTodo = function() {
-//     //     $http.post('/api/todos', $scope.formData)
-//     //         .success(function(data) {
-//     //             $scope.formData = {}; // clear the form so our user is ready to enter another
-//     //             $scope.todos = data;
-//     //             console.log(data);
-//     //         })
-//     //         .error(function(data) {
-//     //             console.log('Error: ' + data);
-//     //         });
-//     // };
-//     //
-//     // // delete a todo after checking it
-//     // $scope.deleteTodo = function(id) {
-//     //     $http.delete('/api/todos/' + id)
-//     //         .success(function(data) {
-//     //             $scope.todos = data;
-//     //             console.log(data);
-//     //         })
-//     //         .error(function(data) {
-//     //             console.log('Error: ' + data);
-//     //         });
-//     // };
-//
-// }
+brawl.directive('modal', function () {
+    return {
+        template: '<div class="modal fade">' +
+            '<div class="modal-dialog">' +
+            '<div class="modal-content">' +
+            '<div class="modal-header">' +
+            // '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
+            '<h4 class="modal-title">{{ title }}</h4>' +
+            '</div>' +
+            '<div class="modal-body" ng-transclude></div>' +
+            '</div>' +
+            '</div>' +
+            '</div>',
+        restrict: 'E',
+        transclude: true,
+        replace: true,
+        scope: true,
+        link: function postLink(scope, element, attrs) {
+            scope.title = attrs.title;
+            scope.classes = attrs.class;
+
+            scope.$watch(attrs.visible, function(value){
+                if(value === true)
+                    $(element).modal('show');
+                else
+                    $(element).modal('hide');
+            });
+
+            $(element).on('shown.bs.modal', function(){
+                scope.$apply(function(){
+                    scope.$parent[attrs.visible] = true;
+                });
+            });
+
+            $(element).on('hidden.bs.modal', function(){
+                scope.$apply(function(){
+                    scope.$parent[attrs.visible] = false;
+                });
+            });
+        }
+    };
+});
